452d2d2b0b78acb9353e212ea74963efbf580ec3
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-01-24 14:33:08 -0500

    Run Prettier on everything

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,27 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from '@/components/Home';
-import Album from '@/components/Album';
-import Artist from '@/components/Artist';
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/components/Home";
+import Album from "@/components/Album";
+import Artist from "@/components/Artist";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: Home,
-    }, {
-      path: '/artist',
-      name: 'Artist',
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/artist",
+      name: "Artist",
       component: Artist
-    }, {
-      path: '/album',
-      name: 'Album',
+    },
+    {
+      path: "/album",
+      name: "Album",
       component: Album
     }
-  ],
+  ]
 });
