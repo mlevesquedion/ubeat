c1b9bd696908bad76f80ae0a781de19c3156ec08
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-01-26 14:07:19 -0500

    Add basic user label component

---
+++ src/components/User.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="columns">
+    <img class="column user-image size-1" src="@/assets/images/profile_pic.jpg">
+    <p class="column">Miaou</p>
+  </div>
+</template>
+
+<style></style>
