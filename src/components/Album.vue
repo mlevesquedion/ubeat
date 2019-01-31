768029a6c0f4ea067887a23bbbc3de53767c6786
David Poisson <david.poisson.1@ulaval.ca>
2019-01-31 07:49:11 -0500

    Progress so far.

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,7 +1,154 @@
 <template>
   <div>
-    <h1>Album</h1>
-    <div>GLO-3102 Album page</div>
+    <section class="hero is-primary ">
+      <div class="hero-body">
+        <div class="container">
+          <figure class="is-left">
+            <p class="image is-128x128">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/en/0/0d/FooFighters-TheColourAndTheShape.jpg"
+              />
+            </p>
+          </figure>
+          <h1 class="title">
+            Foo Fighters
+          </h1>
+          <h2 class="subtitle">
+            The Colour and the Shape
+          </h2>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column"></div>
+          <article class="media">
+            <!--figure class="media-left">
+            <p class="image is-64x64">
+              <img
+                src="https://versions.bulma.io/0.7.0/images/placeholders/128x128.png"
+              />
+            </p>
+          </figure-->
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>1.</strong> <small>Doll</small>
+                  <small>1:23</small>
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                  ornare magna eros, eu pellentesque tortor vestibulum ut.
+                  Maecenas non massa sem. Etiam finibus odio quis feugiat
+                  facilisis.
+                </p>
+              </div>
+              <nav class="level is-mobile">
+                <div class="level-left">
+                  <a class="level-item">
+                    <span class="icon is-small"
+                      ><i class="fas fa-reply"></i
+                    ></span>
+                  </a>
+                  <a class="level-item">
+                    <span class="icon is-small"
+                      ><i class="fas fa-retweet"></i
+                    ></span>
+                  </a>
+                  <a class="level-item">
+                    <span class="icon is-small"
+                      ><i class="fas fa-heart"></i
+                    ></span>
+                  </a>
+                </div>
+              </nav>
+            </div>
+            <div class="media-right">
+              <button class="delete"></button>
+            </div>
+          </article>
+          <article class="media">
+            <!--figure class="media-left">
+            <p class="image is-64x64">
+              <img
+                src="https://versions.bulma.io/0.7.0/images/placeholders/128x128.png"
+              />
+            </p>
+          </figure-->
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>2.</strong> <small>Monkey Wrench</small>
+                  <small>3:51</small>
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                  ornare magna eros, eu pellentesque tortor vestibulum ut.
+                  Maecenas non massa sem. Etiam finibus odio quis feugiat
+                  facilisis.
+                </p>
+              </div>
+              <nav class="level is-mobile">
+                <div class="level-left">
+                  <a class="level-item">
+                    <span class="icon is-small"
+                      ><i class="fas fa-reply"></i
+                    ></span>
+                  </a>
+                  <a class="level-item">
+                    <span class="icon is-small"
+                      ><i class="fas fa-retweet"></i
+                    ></span>
+                  </a>
+                  <a class="level-item">
+                    <span class="icon is-small"
+                      ><i class="fas fa-heart"></i
+                    ></span>
+                  </a>
+                </div>
+              </nav>
+            </div>
+            <div class="media-right">
+              <button class="delete"></button>
+            </div>
+          </article>
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img
+                  src="https://versions.bulma.io/0.7.0/images/placeholders/1280x960.png"
+                  alt="Placeholder image"
+                />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <figure class="image is-48x48">
+                    <img
+                      src="https://versions.bulma.io/0.7.0/images/placeholders/96x96.png"
+                      alt="Placeholder image"
+                    />
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <p class="title is-4">John Smith</p>
+                  <p class="subtitle is-6">@johnsmith</p>
+                </div>
+              </div>
+
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                <a href="#">#css</a>
+                <a href="#">#responsive</a>
+                <br />
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
