7ab73aa4318f7ecbf0ee1221a7aa40b39de439b0
Bruce Perry <qcepicbruce@hotmail.com>
2019-01-30 19:01:08 -0500

    added artist page
    updated styles for uniform label text color

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>
-    <h1>Artist</h1>
-    <div>GLO-3102 Artist page</div>
+  <div class="field has-text-centered">
+    <a class="card-image"
+      ><img
+        class="is-rounded"
+        src="https://i.ibb.co/LPJ7Sxv/Rings-Of-Saturn.jpg"
+        alt="Rings-Of-Saturn"
+        border="0"
+    /></a>
+    <p class="label is-large">Rings of Saturn</p>
+    <p class="label is-large">Technical Death Metal</p>
+
+    <div class="buttons">
+      <button class="button is-fullwidth is-success is-rounded">
+        Ulta Ulla
+      </button>
+      <button class="button is-fullwidth is-success is-rounded">
+        Lugal Ki En
+      </button>
+      <button class="button is-fullwidth is-success is-rounded">
+        Dingir
+      </button>
+      <button class="button is-fullwidth is-success is-rounded">
+        Embryonic Anomaly
+      </button>
+    </div>
+    <a
+      href="https://geo.itunes.apple.com/us/artist/rings-of-saturn/373843476?mt=1&app=music"
+      style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=&kind=artist&bubble=apple_music) no-repeat;width:158px;height:45px;"
+    ></a>
   </div>
 </template>
 
