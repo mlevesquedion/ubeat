9c35259392e31d3a33ca2131adfad05219262e9a
jo-ice <46829447+jo-ice@users.noreply.github.com>
2019-02-06 23:08:08 -0500

    ajout lien pour se déconnecter et paramètres utilisateur pour tablette et mobile

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,7 +2,7 @@
   <nav class="navbar">
     <div class="navbar-brand">
       <router-link id="text-logo" class="is-size-2 navbar-item" to="/"
-        >UBeat</router-link
+      >UBeat</router-link
       >
       <a
         id="burger"
@@ -19,25 +19,32 @@
     </div>
     <div id="nav-menu" class="navbar-menu">
       <div id="nav-start" class="navbar-start">
-        <router-link to="/user" class="navbar-item">Utilisateur</router-link>
-        <div class="navbar-item">
-          <img class="is-rounded" src="@/static/images/default_profile.png" />
-        </div>
-        <div class="navbar-item">
-          <a class="button is-primary is-rounded">
-            <i class="fas fa-sign-out-alt" />
-          </a>
-          <a class="button is-primary is-rounded">
-            <i class="fas fa-cog" />
-          </a>
-        </div>
+
+
       </div>
+
+
+
+
       <div class="navbar-end">
-        <router-link id="user-link" to="/user" class="navbar-item"
-          >User</router-link
-        >
-        <router-link to="/artist" class="navbar-item">Artist</router-link>
-        <router-link to="/album" class="navbar-item">Album</router-link>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+
+          <a class="navbar-link"> <router-link to="/user" class="navbar-item">
+            <img class="is-rounded" src="@/static/images/default_profile.png" />
+            &nbspUser</router-link>
+          </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              <i class="fas fa-cog" />  &nbspSettings
+            </a>
+            <a class="navbar-item">
+              <i class="fas fa-sign-out-alt" /> &nbspSign out
+            </a>
+          </div>
+        </div>
+
         <div class="navbar-item has-dropdown is-hoverable">
           <a id="nav-dropdown" class="navbar-link">Playlists</a>
           <div class="navbar-dropdown">
@@ -59,55 +66,55 @@
 </template>
 
 <script>
-export default {
-  methods: {
-    toggleClassById(id, cls) {
-      const el = document.getElementById(id);
-      el.classList.toggle(cls);
-    },
-    toggleBurger() {
-      this.toggleClassById('burger', 'is-active');
-      this.toggleClassById('nav-menu', 'is-active');
+  export default {
+    methods: {
+      toggleClassById(id, cls) {
+        const el = document.getElementById(id);
+        el.classList.toggle(cls);
+      },
+      toggleBurger() {
+        this.toggleClassById('burger', 'is-active');
+        this.toggleClassById('nav-menu', 'is-active');
+      }
     }
-  }
-};
+  };
 </script>
 
 <style scoped lang="scss">
-@import '@/assets/sass/styles.scss';
-#text-logo {
-  color: $background;
-  font-family: $big-text;
-  background-color: $primary;
-}
-
-#text-logo:hover {
-  color: $background-light;
-}
-
-#text-logo:active {
-  color: $background-lighter;
-}
-
-#nav-menu {
-  padding-top: 0;
-}
-
-#nav-start {
-  display: flex;
-  @media screen and (max-width: $burger-break) {
-    display: none;
+  @import '@/assets/sass/styles.scss';
+  #text-logo {
+    color: $background;
+    font-family: $big-text;
+    background-color: $primary;
   }
-}
 
-#user-link {
-  display: none;
-  @media screen and (max-width: $burger-break) {
+  #text-logo:hover {
+    color: $background-light;
+  }
+
+  #text-logo:active {
+    color: $background-lighter;
+  }
+
+  #nav-menu {
+    padding-top: 0;
+  }
+
+  #nav-start {
     display: flex;
+    @media screen and (max-width: $burger-break) {
+      display: none;
+    }
   }
-}
 
-.navbar-item {
-  @extend .is-unselectable;
-}
+  #user-link {
+    display: none;
+    @media screen and (max-width: $burger-break) {
+      display: flex;
+    }
+  }
+
+  .navbar-item {
+    @extend .is-unselectable;
+  }
 </style>
