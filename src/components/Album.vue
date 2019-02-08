afed5d51d04000b8d2f5925cf53e118c3d5a1aec
David Poisson <david.poisson.1@ulaval.ca>
2019-02-08 08:16:46 -0500

    - Cosmetic changes - Added id's - Added link to iTunes page for album

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -7,52 +7,59 @@
             <div class="media-left">
               <figure class="image is-128x128">
                 <img
+                  id="albumCover"
                   src="https://upload.wikimedia.org/wikipedia/en/8/8b/Foo_Fighters_-_There_Is_Nothing_Left_to_Lose.jpg"
                   alt="Album cover"
                 />
               </figure>
             </div>
             <div class="media-content">
-              <p class="title is-4">There Is Nothing Left to Lose</p>
-              <p class="subtitle is-6">
-                <span>by </span><span>Foo Fighters</span><span> (1998)</span>
+              <p id="albumName" class="title is-4">
+                There Is Nothing Left to Lose
               </p>
-              <p class="subtitle is-6"></p>
+              <p class="subtitle is-6">
+                <span>by </span><span id="artist">Foo Fighters</span
+                ><span id="releaseYear"> (1998)</span>
+              </p>
             </div>
           </div>
 
           <div class="content">
-            <p>
+            <p id="albumDescription">
               There Is Nothing Left to Lose is the third studio album by
               American rock band Foo Fighters, released on November 2, 1999 by
               Roswell and RCA Records.
             </p>
-            <span class="tag is-success is-medium is-rounded">Rock</span>
-            <span class="tag is-success is-medium is-rounded">Alternative</span>
+            <div id="musicStyles">
+              <span class="tag is-success is-medium is-rounded">Rock</span>
+              <span class="tag is-success is-medium is-rounded"
+                >Alternative</span
+              >
+            </div>
           </div>
         </div>
       </div>
       <section class="info-tiles">
         <div class="tile is-ancestor has-text-centered">
           <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">11</p>
+            <article class="tile is-child box is-">
+              <p id="numberSongs" class="title">11</p>
               <p class="subtitle">Songs</p>
             </article>
           </div>
           <div class="tile is-parent">
             <article class="tile is-child box">
-              <p class="title">00:46:19</p>
+              <p id="playTime" class="title">00:46:19</p>
               <p class="subtitle">Play time</p>
             </article>
           </div>
           <div class="tile is-parent">
             <article class="tile is-child box">
-              <figure>
-                <img
-                  src="https://www.apple.com/ca/itunes/link/images/link-badge-itunes.png"
-                />
-              </figure>
+              <a
+                id="albumLink"
+                href="https://itunes.apple.com/ca/album/there-is-nothing-left-to-lose/278229648"
+                style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=&kind=artist&bubble=apple_music) no-repeat;width:158px;height:45px;"
+              ></a>
             </article>
           </div>
         </div>
@@ -382,125 +389,6 @@
               </tr>
             </tbody>
           </table>
-        </div>
-      </div>
-      <!-- Other album by same group -->
-      <div class="container">
-        <div
-          class="carousel carousel-animated carousel-animate-slide"
-          data-size="5"
-        >
-          <div class="carousel-container">
-            <div class="carousel-item is-active">
-              <figure class="image is-2by1">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/9/98/Foo_fighters_in_your_honor.jpg"
-                />
-              </figure>
-            </div>
-            <div class="carousel-item">
-              <figure class="image is-2by1">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/0/0d/FooFighters-TheColourAndTheShape.jpg"
-                />
-              </figure>
-            </div>
-            <div class="carousel-item">
-              <figure class="image is-2by1">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/0/06/Foo_Fighters_-_One_by_One.jpg"
-                />
-              </figure>
-            </div>
-            <div class="carousel-item">
-              <figure class="image is-2by1">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/3/35/FooFightersGreatestHits.jpg"
-                />
-              </figure>
-            </div>
-            <div class="carousel-item">
-              <figure class="image is-2by1">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/0/05/Foo_Fighters_Wasting_Light_Album_Cover.jpg"
-                />
-              </figure>
-            </div>
-          </div>
-          <div class="carousel-navigation is-centered">
-            <div class="carousel-nav-left">
-              <i class="fa fa-chevron-left" aria-hidden="true"></i>
-            </div>
-            <div class="carousel-nav-right">
-              <i class="fa fa-chevron-right" aria-hidden="true"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="container">
-        <div
-          class="carousel carousel-animated carousel-animate-slide"
-          data-size="5"
-        >
-          <div class="carousel-container">
-            <div class="carousel-item is-active">
-              <figure class="image is-2by1">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/5/50/Sgt._Pepper%27s_Lonely_Hearts_Club_Band.jpg"
-                />
-              </figure>
-            </div>
-            <div class="carousel-item">
-              <figure class="image is-2by1">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/b/b7/NirvanaNevermindalbumcover.jpg"
-                />
-              </figure>
-            </div>
-            <div class="carousel-item">
-              <figure class="image is-2by1">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/2/26/Led_Zeppelin_-_Led_Zeppelin_IV.jpg"
-                />
-              </figure>
-            </div>
-            <div class="carousel-item">
-              <figure class="image is-2by1">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png"
-                />
-              </figure>
-            </div>
-            <div class="carousel-item">
-              <figure class="image is-2by1">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/8/84/In_the_Court_of_the_Crimson_King_-_40th_Anniversary_Box_Set_-_Front_cover.jpeg"
-                />
-              </figure>
-            </div>
-            <div class="carousel-item">
-              <figure class="image is-2by1">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/7/7d/Resistance_Is_Futile.jpg"
-                />
-              </figure>
-            </div>
-            <div class="carousel-item">
-              <figure class="image is-2by1">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/2/27/Joe_Satriani_-_2018_-_What_Happens_Next.jpg"
-                />
-              </figure>
-            </div>
-          </div>
-          <div class="carousel-navigation is-centered">
-            <div class="carousel-nav-left">
-              <i class="fa fa-chevron-left" aria-hidden="true"></i>
-            </div>
-            <div class="carousel-nav-right">
-              <i class="fa fa-chevron-right" aria-hidden="true"></i>
-            </div>
-          </div>
         </div>
       </div>
     </div>
