987863f2457b63c6e5b16eb934039a977cfb5207
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-02-08 11:56:25 -0500

    Fix alignment issues, use CSS instead of &nbsp, fix burger menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,9 +1,7 @@
 <template>
   <nav class="navbar">
     <div class="navbar-brand">
-      <router-link id="text-logo" class="is-size-2 navbar-item" to="/"
-      >UBeat</router-link
-      >
+      <router-link id="text-logo" class="is-size-2 navbar-item" to="/">UBeat</router-link>
       <a
         id="burger"
         role="button"
@@ -18,40 +16,37 @@
       </a>
     </div>
     <div id="nav-menu" class="navbar-menu">
-      <div id="nav-start" class="navbar-start">
-
-      </div>
-    </div>
-    <div class="navbar-end">
-      <router-link to="/artist" class="navbar-item">Artist</router-link>
-      <router-link to="/album" class="navbar-item">Album</router-link>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a id="nav-dropdown" class="navbar-link">Playlists</a>
-        <div class="navbar-dropdown">
-          <a class="navbar-item">Playlist 1</a>
-          <a class="navbar-item">Playlist 2</a>
-          <a class="navbar-item">Playlist 3</a>
+      <div class="navbar-end">
+        <div class="navbar-item is-marginless">
+          <input class="is-size-6 search-bar" type="search" placeholder="Search ...">
         </div>
-      </div>
-      <div class="navbar-item">
-        <input
-          type="search"
-          placeholder="Rechercher..."
-          class="navbar-item"
-        />
-      </div>
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link"> <router-link to="/user" class="navbar-item">
-          <img class="is-rounded" src="@/static/images/default_profile.png" />
-          &nbspUser</router-link>
-        </a>
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            <i class="fas fa-cog" />  &nbspSettings
+        <router-link to="/artist" class="navbar-item">Artist</router-link>
+        <router-link to="/album" class="navbar-item">Album</router-link>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a id="nav-dropdown" class="navbar-link">Playlists</a>
+          <div class="navbar-dropdown">
+            <a class="navbar-item">Playlist 1</a>
+            <a class="navbar-item">Playlist 2</a>
+            <a class="navbar-item">Playlist 3</a>
+          </div>
+        </div>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            <router-link to="/user" id="user-container" class="navbar-item">
+              <img class="is-rounded bumped-left" src="@/static/images/default_profile.png">
+              <span>User</span>
+            </router-link>
           </a>
-          <a class="navbar-item">
-            <i class="fas fa-sign-out-alt" /> &nbspSign out
-          </a>
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              <i class="fas fa-cog"/>
+              <span class="bumped-right">Settings</span>
+            </a>
+            <a class="navbar-item">
+              <i class="fas fa-sign-out-alt"/>
+              <span class="bumped-right">Sign out</span>
+            </a>
+          </div>
         </div>
       </div>
     </div>
@@ -59,55 +54,65 @@
 </template>
 
 <script>
-  export default {
-    methods: {
-      toggleClassById(id, cls) {
-        const el = document.getElementById(id);
-        el.classList.toggle(cls);
-      },
-      toggleBurger() {
-        this.toggleClassById('burger', 'is-active');
-        this.toggleClassById('nav-menu', 'is-active');
-      }
+export default {
+  methods: {
+    toggleClassById(id, cls) {
+      const el = document.getElementById(id);
+      el.classList.toggle(cls);
+    },
+    toggleBurger() {
+      this.toggleClassById('burger', 'is-active');
+      this.toggleClassById('nav-menu', 'is-active');
     }
-  };
+  }
+};
 </script>
 
 <style scoped lang="scss">
-  @import '@/assets/sass/styles.scss';
-  #text-logo {
-    color: $background;
-    font-family: $big-text;
-    background-color: $primary;
-  }
+@import '@/assets/sass/styles.scss';
+#text-logo {
+  color: $background;
+  font-family: $big-text;
+  background-color: $primary;
+}
 
-  #text-logo:hover {
-    color: $background-light;
-  }
+#text-logo:hover {
+  color: $background-light;
+}
 
-  #text-logo:active {
-    color: $background-lighter;
-  }
+#text-logo:active {
+  color: $background-lighter;
+}
 
-  #nav-menu {
-    padding-top: 0;
-  }
+#nav-menu {
+  padding-top: 0;
+}
 
-  #nav-start {
+#user-container {
+  @extend .is-paddingless;
+  @media screen and (max-width: $burger-break) {
     display: flex;
-    @media screen and (max-width: $burger-break) {
-      display: none;
-    }
+    align-items: center;
   }
+}
 
-  #user-link {
-    display: none;
-    @media screen and (max-width: $burger-break) {
-      display: flex;
-    }
-  }
+.navbar-item {
+  @extend .is-unselectable;
+}
 
-  .navbar-item {
-    @extend .is-unselectable;
+.bumped-right {
+  margin-left: 10px;
+}
+
+.bumped-left {
+  margin-right: 10px;
+}
+
+.search-bar {
+  height: 40px;
+  padding: 10px;
+  @media screen and (max-width: $burger-break) {
+    width: 100%;
   }
+}
 </style>
