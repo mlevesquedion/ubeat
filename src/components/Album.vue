c2234c7c0072690001e3b0859558bfce5839aa03
jo-ice <46829447+jo-ice@users.noreply.github.com>
2019-02-09 01:17:39 -0500

    suggestion layout

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -59,47 +59,47 @@
 
       <div class="container">
         <section class="section">
-        <div class="columns">
-          <!-- left -->
-          <div class="column">
-            <figure class="media-left">
-              <img
-                style="max-width: 256px"
-                id="albumCover"
-                src="https://upload.wikimedia.org/wikipedia/en/8/8b/Foo_Fighters_-_There_Is_Nothing_Left_to_Lose.jpg"
-                alt="Album cover"
-              />
-            </figure>
-            <div id="musicStyles">
-              <span class="tag is-success is-medium is-rounded">Rock</span>
-              <span class="tag is-success is-medium is-rounded"
-              >Alternative</span
-              >
+          <div class="columns">
+            <!-- left -->
+            <div class="column">
+              <figure class="media-left">
+                <img
+                  style="max-width: 256px"
+                  id="albumCover"
+                  src="https://upload.wikimedia.org/wikipedia/en/8/8b/Foo_Fighters_-_There_Is_Nothing_Left_to_Lose.jpg"
+                  alt="Album cover"
+                />
+              </figure>
+              <div id="musicStyles">
+                <span class="tag is-success is-medium is-rounded">Rock</span>
+                <span class="tag is-success is-medium is-rounded"
+                >Alternative</span
+                >
+              </div>
             </div>
-          </div>
-          <!-- right -->
-          <div class="column is-four-fifths">
-            <p id="albumName" class="title is-1 has-text-light">
-              There Is Nothing Left to Lose
-            </p>
-            <div class="subtitle is-6 has-text-grey-lighter">
-              <p>
-              <span>by </span><span id="artist">Foo Fighters</span
-              ><span id="releaseYear"> (1998)</span>
+            <!-- right -->
+            <div class="column is-four-fifths">
+              <p id="albumName" class="title is-1 has-text-light">
+                There Is Nothing Left to Lose
               </p>
-              <p>
-            <span id="numberSongs">11</span><span> songs, </span>
-            <span id="albumPlayTime">46</span><span> minutes</span>
+              <div class="subtitle is-6 has-text-grey-lighter">
+                <p>
+                  <span>by </span><span id="artist">Foo Fighters</span
+                ><span id="releaseYear"> (1998)</span>
+                </p>
+                <p>
+                  <span id="numberSongs">11</span><span> songs, </span>
+                  <span id="albumPlayTime">46</span><span> minutes</span>
+                </p>
+              </div>
+              <p id="albumDescription">
+                There Is Nothing Left to Lose is the third studio album by
+                American rock band Foo Fighters, released on November 2, 1999 by
+                Roswell and RCA Records.
               </p>
-            </div>
-            <p id="albumDescription">
-              There Is Nothing Left to Lose is the third studio album by
-              American rock band Foo Fighters, released on November 2, 1999 by
-              Roswell and RCA Records.
-            </p>
 
+            </div>
           </div>
-        </div>
         </section>
       </div>
 
@@ -140,242 +140,224 @@
         </div>
       </section-->
       <section class="section">
-      <div id="listSongs" class="content">
-        <ol>
-          <li class="has-background-dark">
-            <div class="level is-mobile is-bordered">
-              <div class="level-left">
-                <div id="songName">Stacked Actors</div>
-                <div>
-                  <span> (</span><span id="playTime">4:17</span><span>)</span>
+        <div id="listSongs" class="content">
+          <ol>
+            <li class="has-background-dark">
+              <div class="level is-mobile is-bordered">
+                <div class="level-left">
+                  <div id="songName">Stacked Actors</div>
                 </div>
-              </div>
-              <div class="level-right">
-                <div class="field is-grouped has-addons">
-                  <p class="control">
-                    <a class="has-text-light">
+                <div class="level-right">
+                  <div class="field is-grouped has-addons">
+                    <p class="control">
+                      <a class="has-text-light">
                       <span class="icon is-medium">
                         <i class="far fa-pause-circle"></i>
                       </span>
-                    </a>
-                  </p>
+                      </a>
+                      <span id="playTime">4:17</span>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <div class="level is-mobile">
-              <div class="level-left">
-                <div id="songName">Breakout</div>
-                <div>
-                  <span> (</span><span id="playTime">3:21</span><span>)</span>
+            </li>
+            <li>
+              <div class="level is-mobile">
+                <div class="level-left">
+                  <div id="songName">Breakout</div>
                 </div>
-              </div>
-              <div class="level-right">
-                <div class="field is-grouped has-addons">
-                  <p class="control">
-                    <a class="has-text-light">
+                <div class="level-right">
+                  <div class="field is-grouped has-addons">
+                    <p class="control">
+                      <a class="has-text-light">
                       <span class="icon is-medium">
                         <i class="far fa-play-circle"></i>
                       </span>
-                    </a>
-                  </p>
+                      </a>
+                      <span id="playTime">3:21</span>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
-          <li class="has-background-dark">
-            <div class="level is-mobile">
-              <div class="level-left">
-                <div id="songName">Learn to Fly</div>
-                <div>
-                  <span> (</span><span id="playTime">3:58</span><span>)</span>
+            </li>
+            <li class="has-background-dark">
+              <div class="level is-mobile">
+                <div class="level-left">
+                  <div id="songName">Learn to Fly</div>
                 </div>
-              </div>
-              <div class="level-right">
-                <div class="field is-grouped has-addons">
-                  <p class="control">
-                    <a class="has-text-light">
+                <div class="level-right">
+                  <div class="field is-grouped has-addons">
+                    <p class="control">
+                      <a class="has-text-light">
                       <span class="icon is-medium">
                         <i class="far fa-play-circle"></i>
                       </span>
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class="level is-mobile">
-              <div class="level-left">
-                <div id="songName">Gimme Stitches</div>
-                <div>
-                  <span> (</span><span id="playTime">3:42</span><span>)</span>
-                </div>
-              </div>
-              <div class="level-right">
-                <div class="field is-grouped has-addons">
-                  <p class="control">
-                    <a class="has-text-light">
-                      <span class="icon is-medium">
-                        <i class="far fa-pause-circle"></i>
-                      </span>
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li class="has-background-dark">
-            <div class="level is-mobile">
-              <div class="level-left">
-                <div id="songName">Generator</div>
-                <div>
-                  <span> (</span><span id="playTime">3:48</span><span>)</span>
-                </div>
-              </div>
-              <div class="level-right">
-                <div class="field is-grouped has-addons">
-                  <p class="control">
-                    <a class="has-text-light">
-                      <span class="icon is-medium">
-                        <i class="far fa-play-circle"></i>
-                      </span>
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class="level is-mobile">
-              <div class="level-left">
-                <div id="songName">Aurora</div>
-                <div>
-                  <span> (</span><span id="playTime">5:49</span><span>)</span>
-                </div>
-              </div>
-              <div class="level-right">
-                <div class="field is-grouped has-addons">
-                  <p class="control">
-                    <a class="icon has-text-light">
-                      <span class="icon is-medium">
-                        <i class="far fa-play-circle"></i>
-                      </span>
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li class="has-background-dark">
-            <div class="level is-mobile">
-              <div class="level-left">
-                <div id="songName">Live-in Skin</div>
-                <div>
-                  <span> (</span><span id="playTime">3:52</span><span>)</span>
-                </div>
-              </div>
-              <div class="level-right">
-                <div class="field is-grouped has-addons">
-                  <p class="control">
-                    <a class="has-text-light">
-                      <span class="icon is-medium">
-                        <i class="far fa-play-circle"></i>
-                      </span>
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class="level is-mobile">
-              <div class="level-left">
-                <div id="songName">Next Year</div>
-                <div>
-                  <span> (</span><span id="playTime">4:37</span><span>)</span>
-                </div>
-              </div>
-              <div class="level-right">
-                <div class="field is-grouped has-addons">
-                  <p class="control">
-                    <a class="has-text-light">
-                      <span class="icon is-medium">
-                        <i class="far fa-play-circle"></i>
-                      </span>
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li class="has-background-dark">
-            <div class="level is-mobile">
-              <div class="level-left">
-                <div id="songName">Headwires</div>
-                <div>
-                  <span> (</span><span id="playTime">4:37</span><span>)</span>
-                </div>
-              </div>
-              <div class="level-right">
-                <div class="field is-grouped has-addons">
-                  <p class="control">
-                    <a class="has-text-light">
-                      <span class="icon is-medium">
-                        <i class="far fa-play-circle"></i>
-                      </span>
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class="level is-mobile">
-              <div class="level-left">
-                <div id="songName">Ain't it the Life</div>
-                <div>
-                  <span> (</span><span id="playTime">4:15</span><span>)</span>
-                </div>
-              </div>
-              <div class="level-right">
-                <div class="field is-grouped has-addons">
-                  <p class="control">
-                    <a class="has-text-light">
-                      <span class="icon is-medium">
-                        <i class="far fa-play-circle"></i>
-                      </span>
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li class="has-background-dark">
-            <div class="level is-mobile">
-              <div class="level-left">
-                <div id="songName">M.I.A</div>
-                <div>
-                  <span> (</span><span id="playTime">4:03</span><span>)</span>
-                </div>
-              </div>
-              <div class="level-right">
-                <div class="field is-grouped has-addons">
-                  <p class="control">
-                    <a class="has-text-light">
-                      <span class="icon is-medium">
-                        <i class="far fa-play-circle"></i>
-                      </span>
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ol>
+                      </a>
+                      <span id="playTime">3:58</span>
 
-      </div>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="level is-mobile">
+                <div class="level-left">
+                  <div id="songName">Gimme Stitches</div>
+                </div>
+                <div class="level-right">
+                  <div class="field is-grouped has-addons">
+                    <p class="control">
+                      <a class="has-text-light">
+                      <span class="icon is-medium">
+                        <i class="far fa-pause-circle"></i>
+                      </span>
+                      </a>
+                      <span id="playTime">3:42</span>
+
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="has-background-dark">
+              <div class="level is-mobile">
+                <div class="level-left">
+                  <div id="songName">Generator</div>
+                </div>
+                <div class="level-right">
+                  <div class="field is-grouped has-addons">
+                    <p class="control">
+                      <a class="has-text-light">
+                      <span class="icon is-medium">
+                        <i class="far fa-play-circle"></i>
+                      </span>
+                      </a>
+                      <span id="playTime">3:48</span>
+                    </p>
+                  </div>
+
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="level is-mobile">
+                <div class="level-left">
+                  <div id="songName">Aurora</div>
+                </div>
+                <div class="level-right">
+                  <div class="field is-grouped has-addons">
+                    <p class="control">
+                      <a class="has-text-light">
+                      <span class="icon is-medium">
+                        <i class="far fa-play-circle"></i>
+                      </span>
+                      </a>
+                      <span id="playTime">5:49</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="has-background-dark">
+              <div class="level is-mobile">
+                <div class="level-left">
+                  <div id="songName">Live-in Skin</div>
+                </div>
+                <div class="level-right">
+                  <div class="field is-grouped has-addons">
+                    <p class="control">
+                      <a class="has-text-light">
+                      <span class="icon is-medium">
+                        <i class="far fa-play-circle"></i>
+                      </span>
+                      </a>
+                      <span id="playTime">3:52</span>
+
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="level is-mobile">
+                <div class="level-left">
+                  <div id="songName">Next Year</div>
+                </div>
+                <div class="level-right">
+                  <div class="field is-grouped has-addons">
+                    <p class="control">
+                      <a class="has-text-light">
+                      <span class="icon is-medium">
+                        <i class="far fa-play-circle"></i>
+                      </span>
+                      </a>
+                      <span id="playTime">4:37</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="has-background-dark">
+              <div class="level is-mobile">
+                <div class="level-left">
+                  <div id="songName">Headwires</div>
+                </div>
+                <div class="level-right">
+                  <div class="field is-grouped has-addons">
+                    <p class="control">
+                      <a class="has-text-light">
+                      <span class="icon is-medium">
+                        <i class="far fa-play-circle"></i>
+                      </span>
+                      </a>
+                      <span id="playTime">4:37</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="level is-mobile">
+                <div class="level-left">
+                  <div id="songName">Ain't it the Life</div>
+                </div>
+                <div class="level-right">
+                  <div class="field is-grouped has-addons">
+                    <p class="control">
+                      <a class="has-text-light">
+                      <span class="icon is-medium">
+                        <i class="far fa-play-circle"></i>
+                      </span>
+                      </a>
+                      <span id="playTime">4:15</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="has-background-dark">
+              <div class="level is-mobile">
+                <div class="level-left">
+                  <div id="songName">M.I.A</div>
+                </div>
+                <div class="level-right">
+                  <div class="field is-grouped has-addons">
+                    <p class="control">
+                      <a class="has-text-light">
+                      <span class="icon is-medium">
+                        <i class="far fa-play-circle"></i>
+                      </span>
+                      </a>
+                      <span id="playTime">4:03</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ol>
+
+        </div>
       </section>
       <footer class="footer" style="background-color:#27272b">
         <div class="has-text-centered">
