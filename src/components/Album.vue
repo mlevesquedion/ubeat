7e3342cb172e6d3feab676e668d22e4fbc0033f4
David Poisson <david.poisson.1@ulaval.ca>
2019-02-08 20:42:37 -0500

    - Regress so far

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <div class="container">
+      <!--
       <div class="card">
         <div class="card-content">
           <div class="media">
             <div class="media-left">
-              <figure class="image is-128x128">
+              <figure class="image is-256x256">
                 <img
                   id="albumCover"
                   src="https://upload.wikimedia.org/wikipedia/en/8/8b/Foo_Fighters_-_There_Is_Nothing_Left_to_Lose.jpg"
@@ -21,28 +22,101 @@
                 <span>by </span><span id="artist">Foo Fighters</span
                 ><span id="releaseYear"> (1998)</span>
               </p>
-            </div>
-          </div>
-
-          <div class="content">
-            <p id="albumDescription">
-              There Is Nothing Left to Lose is the third studio album by
-              American rock band Foo Fighters, released on November 2, 1999 by
-              Roswell and RCA Records.
-            </p>
-            <div id="musicStyles">
-              <span class="tag is-success is-medium is-rounded">Rock</span>
-              <span class="tag is-success is-medium is-rounded"
-                >Alternative</span
-              >
+              <div class="content">
+                <p id="albumDescription">
+                  There Is Nothing Left to Lose is the third studio album by
+                  American rock band Foo Fighters, released on November 2, 1999
+                  by Roswell and RCA Records.
+                </p>
+                <div id="musicStyles">
+                  <span class="tag is-success is-medium is-rounded">Rock</span>
+                  <span class="tag is-success is-medium is-rounded"
+                    >Alternative</span
+                  >
+                </div>
+                <div class="box">
+                  <a
+                    id="albumLink"
+                    href="https://itunes.apple.com/ca/album/there-is-nothing-left-to-lose/278229648"
+                    style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=&kind=artist&bubble=apple_music) no-repeat;width:158px;height:45px;"
+                  ></a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <section class="info-tiles">
+      -->
+      <!--div class="media-left">
+        <figure class="image is-256x256">
+          <img
+            id="albumCover"
+            src="https://upload.wikimedia.org/wikipedia/en/8/8b/Foo_Fighters_-_There_Is_Nothing_Left_to_Lose.jpg"
+            alt="Album cover"
+          />
+        </figure>
+      </div-->
+
+      <div class="container">
+        <div class="">
+          <!-- left -->
+          <div class="column">
+            <figure class="image">
+              <img
+                style="max-width: 256px"
+                id="albumCover"
+                src="https://upload.wikimedia.org/wikipedia/en/8/8b/Foo_Fighters_-_There_Is_Nothing_Left_to_Lose.jpg"
+                alt="Album cover"
+              />
+            </figure>
+          </div>
+          <!-- right -->
+          <div class="column">
+            <div class="container">
+              <p id="albumName" class="title is-4">
+                There Is Nothing Left to Lose
+              </p>
+              <p class="subtitle is-6">
+                <span>by </span><span id="artist">Foo Fighters</span
+                ><span id="releaseYear"> (1998)</span>
+              </p>
+              <p id="albumDescription">
+                There Is Nothing Left to Lose is the third studio album by
+                American rock band Foo Fighters, released on November 2, 1999 by
+                Roswell and RCA Records.
+              </p>
+
+              <div id="musicStyles">
+                <span class="tag is-success is-medium is-rounded">Rock</span>
+                <span class="tag is-success is-medium is-rounded"
+                  >Alternative</span
+                >
+              </div>
+              <a
+                id="albumLink"
+                href="https://itunes.apple.com/ca/album/there-is-nothing-left-to-lose/278229648"
+                style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=&kind=artist&bubble=apple_music) no-repeat;width:158px;height:45px;"
+              ></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="level">
+        <div class="level-item box has-text-centered">
+          <p id="numberSongs" class="title">11</p>
+          <p class="subtitle">Songs</p>
+        </div>
+        <div class="level-item box has-text-centered">
+          <p id="albumPlayTime" class="title">00:46:19</p>
+          <p class="subtitle">Play time</p>
+        </div>
+      </div>
+
+      <!--section class="info-tiles">
         <div class="tile is-ancestor has-text-centered">
           <div class="tile is-parent">
-            <article class="tile is-child box is-">
+            <article class="tile is-child box">
               <p id="numberSongs" class="title">11</p>
               <p class="subtitle">Songs</p>
             </article>
@@ -63,7 +137,7 @@
             </article>
           </div>
         </div>
-      </section>
+      </section-->
       <div id="listSongs" class="content">
         <ol type="1">
           <li>
