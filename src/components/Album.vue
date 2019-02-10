b171ffb3bbd4dd8a091321f92f8383a0fd12f358
jo-ice <46829447+jo-ice@users.noreply.github.com>
2019-02-10 14:01:09 -0500

    Lien itunes

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -34,7 +34,7 @@
     </section>
     <footer class="footer has-text-centered">
       <a href="https://itunes.apple.com/ca/album/there-is-nothing-left-to-lose/278229648?app=itunes">
-        <img src="@/static/images/get_on_itunes.png" alt="There Is Nothing Left to Lose">
+        <img src="@/static/images/get_on_itunes.png" alt="iTunes">
       </a>
     </footer>
   </div>
