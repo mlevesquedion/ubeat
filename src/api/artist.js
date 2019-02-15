d13514dc0367aac03a2f17e0b11ee4e7da5db8ff
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-02-15 18:35:08 -0500

    Add imports and exports

--- src/api/artist.js
+++ src/api/artist.js
@@ -1,11 +1,18 @@
-import { apiRoot } from './constants';
+import axios from 'axios';
+import { apiRoot } from './common';
 
-const artistRoot = apiRoot + 'artists/';
+const artistRoot = `${apiRoot}artists/`;
 
-const getArtist = (id, callBack) => {
-  axios.get(artistRoot + id).then(callBack);
+export const getArtist = (id, callBack, onError) => {
+  axios
+    .get(`${artistRoot}${id}`)
+    .then(({ data }) => callBack(data.results[0]))
+    .catch(onError);
 };
 
-const getArtistAlbums = (id, callBack) => {
-  axios.get(artistRoot + id + '/albums').then(callBack);
+export const getArtistAlbums = (id, callBack, onError) => {
+  axios
+    .get(`${artistRoot}${id}/albums`)
+    .then(({ data }) => callBack(data.results))
+    .catch(onError);
 };
