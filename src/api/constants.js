c13fc70c2d79234845490dca3f725b9dd26919db
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-02-15 18:36:22 -0500

    Add exports and RequestState enum

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,1 +1,7 @@
-const apiRoot = 'http://ubeat.herokuapp.com/unsecure/';
+export const apiRoot = 'http://ubeat.herokuapp.com/unsecure/';
+
+export const RequestState = Object.freeze({
+  LOADING: Symbol('loading'),
+  LOADED: Symbol('loaded'),
+  ERROR: Symbol('error')
+});
