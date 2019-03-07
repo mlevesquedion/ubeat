bead912872919b972eed8efe6cb3c9b8ad99975e
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-03-07 09:44:30 -0500

    Extract track duration formatting to a function (not the ViewModel's responsibility) and test it

---
+++ src/formatting/trackDurationFormatter.js
@@ -0,0 +1,9 @@
+export default {
+  format: seconds => {
+    const minutes = Math.floor(seconds / 60);
+    const leftOverSeconds = Math.floor(seconds % 60);
+    const formattedSeconds = leftOverSeconds.toString()
+      .padStart(2, '0');
+    return `${minutes}:${formattedSeconds}`;
+  }
+};
