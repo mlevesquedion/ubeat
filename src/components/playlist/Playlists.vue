88b1c74945cc3e58135c42069e3bf623d41d96c1
Samuel <sammorngam@hotmail.com>
2019-03-07 13:37:15 -0500

    create playlist

--- src/components/playlist/Playlists.vue
+++ src/components/playlist/Playlists.vue
@@ -6,8 +6,10 @@
       <div class="columns">
         <div class="column is-one-third is-3-fullhd">
 
-          <p id="create-message" class="is-primary is-size-4-desktop is-size-5-tablet is-size-6-mobile"> Create a new
-            playlist</p>
+          <button @click="createPlaylist" id="create-message"
+                  class="is-primary is-size-4-desktop is-size-5-tablet is-size-6-mobile"> Create a new
+            playlist
+          </button>
           <span> <p id="existing-message" class="is-primary is-size-4-desktop is-size-5-tablet is-size-6-mobile">
               Your playlists :
             </p>
@@ -17,34 +19,46 @@
       </div>
     </section>
 
-
     <Async :dataSource="dataSource" :dataName="dataName">
       <template slot-scope="{data}">
         <PlaylistView :playlist="data"/>
+        <PlaylistTracks :playlist="data"/>
       </template>
     </Async>
-
 
   </div>
 </template>
 
 <script>
-  import { getPlaylist } from '@/api/playlist';
-  import Async from '@/components/utils/Async/Async';
-  import PlaylistView from '@/components/PlaylistView';
+  import { getPlaylist, getUserPlaylists, postPlaylist } from '@/api/playlist';
+  import Async from '../utils/Async/Async';
+  import PlaylistView from './PlaylistView';
+  import PlaylistTracks from './PlaylistTracks';
 
   export default {
     name: 'playlist',
     data() {
       return {
         dataSource:
-          getPlaylist('5c7f1e7483f8840004680d0e'),
-        dataName: 'user playlist'
+          getPlaylist(this.playlistId),
+        dataName: 'that playlist',
+        playlistId: '5c8130f8d6f63a0004c2653a',
+      }, {
+        dataSource:
+          getUserPlaylists(this.ownerEmail),
+        dataName: 'user playlists',
+        ownerEmail: 'utilisateur@gmail.com'
       };
+    },
+    methods: {
+      createPlaylist() {
+        postPlaylist(data => alert(data), err => alert(err));
+      }
     },
     components: {
       Async,
-      PlaylistView
+      PlaylistView,
+      PlaylistTracks
     }
   };
 </script>
