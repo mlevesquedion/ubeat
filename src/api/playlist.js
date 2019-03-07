f358c211353a8604c01d1c4e05b81c671cca8baa
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-03-07 13:44:04 -0500

    Reintroduce old code that was overwritten

--- src/api/playlist.js
+++ src/api/playlist.js
@@ -26,3 +26,41 @@ const api = {
 };
 
 export default api;
+
+const oldCode = `import axios from 'axios';
+import { apiRoot } from './constants';
+import Playlist from '../models/playlist';
+
+const playlistRoot = `;${apiRoot}
+playlists / `;
+const axiosConfig = {
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Access-Control-Allow-Origin': '*'
+  }
+};
+const ownerEmail = 'utilisateur@gmail.com';
+
+export const getPlaylist = id => (callBack, onError) => {
+  axios
+    .get(`${playlistRoot}${id}`)
+    .then(({ data }) => callBack(Playlist.from(data)))
+    .catch(onError);
+};
+
+export const getUserPlaylists = (callBack, onError) => {
+  axios
+    .get(`${playlistRoot}`)
+    .then(({ data }) => data.filter(p => p.owner.email === ownerEmail))
+    .then(userPlaylists => callBack(userPlaylists))
+
+    .catch(onError);
+};
+
+export const postPlaylist = (callBack, onError) => {
+  axios
+    .post(`${playlistRoot}`, { name: 'unePlaylist513513463', owner: ownerEmail }, axiosConfig)
+    .then(({ data }) => callBack(Playlist.from(data)))
+    .catch(onError);
+};
+`;
