dea0b0cacbed050657f5e73a4bafd96453bbf45b
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-03-12 16:31:41 -0400

    Add missing key in v-for

--- src/components/playlists/PlaylistList.vue
+++ src/components/playlists/PlaylistList.vue
@@ -2,18 +2,14 @@
   <section class="section">
     <h1 class="title has-text-light">Playlists</h1>
     <div v-if="hasPlaylists" class="accordions">
-      <Accordion
-        v-for="(p, pi) in playlists"
-        :key="p.id"
-      >
+      <Accordion v-for="(p, pi) in playlists" :key="p.id">
         <template slot="header">
           <div class="level is-mobile">
             <div class="level-left">
               <span class="subtitle is-primary level-item">{{p.name}}</span>
             </div>
             <div class="level-right">
-              <button class="button level-item is-danger"
-                      @click.stop="deletePlaylist(p.id, pi)">
+              <button class="button level-item is-danger" @click.stop="deletePlaylist(p.id, pi)">
                 <i class="fas fa-trash"></i>
               </button>
             </div>
@@ -21,16 +17,16 @@
         </template>
         <template slot="body">
           <div v-if="empty(p)">This playlist is empty!</div>
-          <div class="level is-mobile" v-for="(t, ti) in p.tracks">
+          <div class="level is-mobile" v-for="(t, ti) in p.tracks" :key="t.id">
             <div class="level-left">
-              <div class="level-item">
-                {{ t.name }}, {{ t.artist }}
-              </div>
+              <div class="level-item">{{ t.name }}, {{ t.artist }}</div>
             </div>
             <div class="level-right">
               <div class="level-item">
-                <button class="button level-item is-danger is-small"
-                        @click="deleteTrack(p.id, pi, t.id, ti)">
+                <button
+                  class="button level-item is-danger is-small"
+                  @click="deleteTrack(p.id, pi, t.id, ti)"
+                >
                   <i class="fas fa-trash"></i>
                 </button>
               </div>
@@ -39,44 +35,44 @@
         </template>
       </Accordion>
     </div>
-    <div v-else>
-      You have no playlists!
-    </div>
+    <div v-else>You have no playlists!</div>
   </section>
 </template>
 
 <script>
-  import isEmpty from '@/utils/isEmpty';
-  import Accordion from '../utils/Accordion';
-  import PlaylistAPI from '../../api/playlists';
+import isEmpty from '@/utils/isEmpty';
+import Accordion from '../utils/Accordion';
+import PlaylistAPI from '../../api/playlists';
 
-  export default {
-    name: 'PlaylistList',
-    props: ['playlists'],
-    computed: {
-      hasPlaylists() {
-        return !isEmpty(this.playlists);
-      }
+export default {
+  name: 'PlaylistList',
+  props: ['playlists'],
+  computed: {
+    hasPlaylists() {
+      return !isEmpty(this.playlists);
+    }
+  },
+  methods: {
+    empty(playlist) {
+      return isEmpty(playlist.tracks);
     },
-    methods: {
-      empty(playlist) {
-        return isEmpty(playlist.tracks);
-      },
-      deletePlaylist(playlistId, playlistIndex) {
-        PlaylistAPI.delete(playlistId)
-          .then(_ => this.$emit('delete-playlist', playlistIndex));
-      },
-      deleteTrack(playlistId, playlistIndex, trackId, trackIndex) {
-        PlaylistAPI.deleteTrack(playlistId, trackId)
-          .then(_ => this.$emit('delete-track', playlistIndex, trackIndex));
-      }
+    deletePlaylist(playlistId, playlistIndex) {
+      PlaylistAPI.delete(playlistId).then(_ =>
+        this.$emit('delete-playlist', playlistIndex)
+      );
     },
-    components: { Accordion }
-  };
+    deleteTrack(playlistId, playlistIndex, trackId, trackIndex) {
+      PlaylistAPI.deleteTrack(playlistId, trackId).then(_ =>
+        this.$emit('delete-track', playlistIndex, trackIndex)
+      );
+    }
+  },
+  components: { Accordion }
+};
 </script>
 
 <style scoped lang="scss">
-  .section {
-    padding-top: 0;
-  }
+.section {
+  padding-top: 0;
+}
 </style>
