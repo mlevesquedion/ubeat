64408ec0c46361f99d0fa04fd604c872c1a500ff
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-03-12 16:32:45 -0400

    Remove tests from User page

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,23 +2,10 @@
   <div>
     <h1>User</h1>
     <div>GLO-3102 User page</div>
-    <button @click="showSuccess">Succeed</button>
-    <button @click="showError">Fail</button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'user',
-  methods: {
-    showSuccess() {
-      this.$toasted.show('Success', { type: 'ubeat-success' });
-    },
-    showError() {
-      this.$toasted.show('Error', { type: 'ubeat-error' });
-    }
-  }
-};
 </script>
 
 <style></style>
