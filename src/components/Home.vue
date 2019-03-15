d2efa94479301c983cb2c8728ae144466e987a2f
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-03-15 17:10:31 -0400

    Fix link alignment

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,35 +1,31 @@
 <template>
-  <div>
-    <div>
-      <header>
-        <h1
-          id="welcome-message"
-          class="title is-primary is-size-1-desktop is-size-2-tablet is-size-4-mobile flex-centered"
-        >
-          Welcome to UBeat!
-        </h1>
-        <p
-          id="summary-message"
-          class="is-size-3-desktop is-size-4-tablet is-size-5-mobile is-primary flex-centered"
-        >
-          Manage your favorite albums, artists, playlists and tracks. UBeat does
-          it all and lets you listen to your music, your way.
-        </p>
-        <ImageCarousel />
-        <div class="links">
-          <router-link to="/artist/491675603" class="responsive-heading"
-            >Artist
-          </router-link>
-          <router-link to="/album/1083396755" class="responsive-heading"
-            >Album
-          </router-link>
-          <router-link to="/playlists" class="responsive-heading"
-            >Playlists
-          </router-link>
-        </div>
-      </header>
+  <section class="section">
+    <h1
+      id="welcome-message"
+      class="title is-primary is-size-1-desktop is-size-2-tablet is-size-4-mobile flex-centered"
+    >
+      Welcome to UBeat!
+    </h1>
+    <p
+      id="summary-message"
+      class="is-size-3-desktop is-size-4-tablet is-size-5-mobile is-primary flex-centered"
+    >
+      Manage your favorite albums, artists, playlists and tracks. UBeat does it
+      all and lets you listen to your music, your way.
+    </p>
+    <ImageCarousel />
+    <div class="links">
+      <router-link to="/artist/491675603" class="responsive-heading"
+        >Artist
+      </router-link>
+      <router-link to="/album/1083396755" class="responsive-heading"
+        >Album
+      </router-link>
+      <router-link to="/playlists" class="responsive-heading"
+        >Playlists
+      </router-link>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -60,10 +56,10 @@ export default {
 .links {
   padding-top: 1em;
   display: flex;
-  justify-content: center;
-}
-
-.links > * {
-  margin-left: 20px;
+  justify-content: space-evenly;
+  min-width: 250px;
+  max-width: 600px;
+  width: 50vw;
+  margin: 0 auto;
 }
 </style>
