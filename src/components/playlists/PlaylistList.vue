38c6d70a21e38181be330e844cc1047ccfe0f1c0
jo-ice <joice.kariya.1@ulaval.ca>
2019-03-16 01:02:49 -0400

    Sort playlist by name

--- src/components/playlists/PlaylistList.vue
+++ src/components/playlists/PlaylistList.vue
@@ -5,7 +5,7 @@
       <Playlist
         :playlist="p"
         :index="pi"
-        v-for="(p, pi) in playlists"
+        v-for="(p, pi) in sortedPlaylist"
         :key="p.id"
       />
     </div>
@@ -14,28 +14,40 @@
 </template>
 
 <script>
-import isEmpty from '@/utils/isEmpty';
-import Playlist from './Playlist';
+  import isEmpty from '@/utils/isEmpty';
+  import Playlist from './Playlist';
 
-export default {
-  name: 'PlaylistList',
-  props: ['playlists'],
-  data() {
-    return {
-      noPlaylistsMessage: 'You have no playlists!'
-    };
-  },
-  computed: {
-    hasPlaylists() {
-      return !isEmpty(this.playlists);
-    }
-  },
-  components: { Playlist }
-};
+  export default {
+    name: 'PlaylistList',
+    props: ['playlists'],
+    data() {
+      return {
+        noPlaylistsMessage: 'You have no playlists!'
+      };
+    },
+    computed: {
+      hasPlaylists() {
+        return !isEmpty(this.playlists);
+      },
+      sortedPlaylist() {
+        function compare(a, b) {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        }
+        return this.playlists.sort(compare);
+      },
+    },
+    components: { Playlist }
+  };
 </script>
 
 <style scoped lang="scss">
-.section {
-  padding-top: 0;
-}
+  .section {
+    padding-top: 0;
+  }
 </style>
