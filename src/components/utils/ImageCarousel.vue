beaace79f3a830e6ea99383efb2be25e091890af
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-03-17 13:37:48 -0400

    Refactor into legitimate component

--- src/components/utils/ImageCarousel.vue
+++ src/components/utils/ImageCarousel.vue
@@ -1,28 +1,41 @@
 <template>
   <div class="flex-centered">
-    <div id="carousel"></div>
+    <!-- Have to use inline style here to achieve transition effect -->
+    <div id="carousel" :style="{ 'background-image': `url(${image})` }"></div>
   </div>
 </template>
 
 <script>
-const images = [
-  'https://www.billboard.com/files/styles/900_wide/public/media/Joy-Division-Unknown-Pleasures-album-covers-billboard-1000x1000.jpg',
-  'https://3m84r11gpx1j11puas2g5wfl-wpengine.netdna-ssl.com/wp-content/uploads/2015/12/Tame-Impala-Currents.jpg',
-  'https://blurppy.files.wordpress.com/2016/02/deftones-gore-album-cover.jpg?w=900',
-  'https://i1.wp.com/vehlinggo.com/wp-content/uploads/2018/07/GUNSHIP_Dark_All_Day_Album_Pack_Shot.jpg?ssl=1',
-  'https://lastfm-img2.akamaized.net/i/u/ar0/6cdffee4073d4859858256a103a75591',
-  'http://truomega.ca/wp-content/uploads/2015/12/Jingle-Cats-Album-Cover.png'
-];
-let imageIndex = 1;
-const updateImage = () => {
-  const carousel = document.getElementById('carousel');
-  if (carousel !== null) {
-    const image = images[imageIndex];
-    carousel.style.backgroundImage = `url(${image})`;
-    imageIndex = (imageIndex + 1) % images.length;
+export default {
+  data() {
+    return {
+      images: [
+        'https://www.billboard.com/files/styles/900_wide/public/media/Joy-Division-Unknown-Pleasures-album-covers-billboard-1000x1000.jpg',
+        'https://www.billboard.com/files/styles/900_wide/public/media/Joy-Division-Unknown-Pleasures-album-covers-billboard-1000x1000.jpg',
+        'https://3m84r11gpx1j11puas2g5wfl-wpengine.netdna-ssl.com/wp-content/uploads/2015/12/Tame-Impala-Currents.jpg',
+        'https://blurppy.files.wordpress.com/2016/02/deftones-gore-album-cover.jpg?w=900',
+        'https://i1.wp.com/vehlinggo.com/wp-content/uploads/2018/07/GUNSHIP_Dark_All_Day_Album_Pack_Shot.jpg?ssl=1',
+        'https://lastfm-img2.akamaized.net/i/u/ar0/6cdffee4073d4859858256a103a75591',
+        'http://truomega.ca/wp-content/uploads/2015/12/Jingle-Cats-Album-Cover.png'
+      ],
+      imageIndex: 0
+    };
+  },
+  computed: {
+    image() {
+      return this.images[this.imageIndex];
+    }
+  },
+  methods: {
+    updateImage() {
+      this.imageIndex = (this.imageIndex + 1) % this.images.length;
+      console.log(this.image);
+    }
+  },
+  created() {
+    setInterval(this.updateImage, 3000);
   }
 };
-setInterval(updateImage, 3000);
 </script>
 
 <style scoped lang="scss">
@@ -37,7 +50,6 @@ setInterval(updateImage, 3000);
   background-size: contain;
   background-repeat: no-repeat;
   background-position: center;
-  background-image: url('https://www.billboard.com/files/styles/900_wide/public/media/Joy-Division-Unknown-Pleasures-album-covers-billboard-1000x1000.jpg');
   transition: background-image 1s ease-in-out;
 }
 
