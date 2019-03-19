006098782b85ccb88b8c11d40a36bef2fbd29e7e
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-03-19 19:50:24 -0400

    Login by default

--- src/components/auth/Auth.vue
+++ src/components/auth/Auth.vue
@@ -118,7 +118,7 @@ export default {
       password: '',
       passwordError: '',
       passwordWasBlurred: false,
-      isLogin: false,
+      isLogin: true,
       isSkipping: false
     };
   },
