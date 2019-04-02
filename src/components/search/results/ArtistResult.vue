362a84b71c8081355cfd24c59e4471eac738e2a9
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-04-02 16:53:54 -0400

    Make it look better

--- src/components/search/results/ArtistResult.vue
+++ src/components/search/results/ArtistResult.vue
@@ -1,30 +1,19 @@
 <template>
   <div class="level is-mobile is-bordered">
-    <div class="level-left is-flex">
+    <div class="level-left">
       <router-link
         class="is-primary link"
         :to="{ name: 'Artist', params: { id: artist.id } }"
         >{{ artist.name }}
       </router-link>
-      <div id="genre" class="has-text-light">, Genre : {{ artist.genre }}</div>
     </div>
+    <div class="level-right has-text-light">{{ artist.genre }}</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ArtistResult',
-  props: ['artist'],
-  mounted() {
-    console.log(this.artist);
-  }
+  props: ['artist']
 };
 </script>
-
-<style scoped lang="scss">
-.is-flex {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-}
-</style>
