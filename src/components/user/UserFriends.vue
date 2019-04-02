9ef91b6424d1ff751e8c68228ee6e317ba0fd3a4
jo-ice <joice.kariya.1@ulaval.ca>
2019-04-01 22:26:09 -0400

    Page utilisateur

--- src/components/user/UserFriends.vue
+++ src/components/user/UserFriends.vue
@@ -1,13 +1,129 @@
 <template>
-
+  <div class="container">
+    <div class="hero-body">
+      <div class="columns is-mobile is-multiline is-centered ">
+        <div v-for="friend in user1.following" class=" ">
+          <button class="column friend-card btn"
+          >
+            <div class="friend-initial"><p>{{friend.name[0]}}</p></div>
+            <div class="friend-name">{{friend.name}}</div>
+            <div class="follow-user-friend">
+              <div v-if="friend.isFollowing"> <i class="fas fa-check add-friend"></i> </div>
+              <button v-else> <i class="fas fa-user-plus"></i> Follow </button>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+  import userTemp from './userTemporaire.json';
+
   export default {
-    name: 'UserFriends'
+    name: 'UserFriends',
+    props: ['friend', 'index', 'userList'],
+    data() {
+      return {
+        user1: userTemp,
+        isUnfollowing: false
+      };
+    },
+    methods: {
+    },
   };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @import '@/assets/sass/styles.scss';
+
+  .friend-card{
+    color: white;
+    border-radius: 2px;
+    margin: 20px;
+    display: block;
+    padding: 20px;
+    text-align: center;
+    min-width: 200px;
+    background-color: #242424;
+    border: None;
+  }
+
+
+  .friend-card:hover{
+    background-color: dimgrey;
+  }
+
+
+  .friend-content{
+    background-color: #181818;
+    border: 1px solid #535353;
+    border-top: 0;
+  }
+
+
+  .check-icon {
+    float: left;
+    padding: 0 10px 0 0;
+    font-size: 10px;
+    margin-top: 10px;
+  }
+
+  .friend-initial{
+    margin: auto;
+    position: relative;
+    font-size: 25px;
+    background-color: grey;
+    height: 100px;
+    width: 100px;
+    border-radius: 50%;
+    text-align: center;
+    vertical-align: middle;
+  }
+
+  .friend-initial p {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .follow-check{
+    position: absolute;
+    top: 15px;
+    margin-right: 10px;
+  }
+
+  .friend-name{
+    margin-top: 20px;
+    font-size: 16px;
+  }
+
+  .friend-email{
+    font-size: 10px
+  }
+
+
+  .follow-user-friend{
+    margin-top: 10px;
+    color: $primary;
+  }
+
+  .follow-user-friend button{
+    color: white;
+    border: none;
+    background-color: $primary;
+  }
+
+  #user-image {
+    display: flex;
+    width: 100px;
+  }
+
+  .add-friend{
+    height: 20px
+  }
 
 </style>
--- src/components/user/UserPlaylist.vue
+++ src/components/user/UserPlaylist.vue
@@ -1,13 +1,86 @@
 <template>
+  <div class="container">
+    <div class="hero-body playlist-content">
+      <div v-for="playlist in user1.userplaylist">
+        <div class="playlist-name row">{{getPlaylistName(playlist)}}
+        </div>
+        <div v-for="song in getPlaylistContent(playlist)">
+          <div class="row">
+            <div class="column check-icon">
+              <div v-if="song.isInPlaylist"> <i class="fas fa-check "></i> </div>
+              <button v-else class="plus-icon"> <i class="fas fa-plus"></i> </button></div>
+            <div class="column song-name">{{song.name}}</div>
+          </div>
+          <div class="song-info">{{song.artist}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 </template>
 
 <script>
+  import userTemp from './userTemporaire.json';
+
   export default {
-    name: 'UserPlaylist'
+    name: 'UserPlaylist',
+    data() {
+      return {
+        user1: userTemp,
+      };
+    },
+    methods: {
+      getPlaylistName(playlist) {
+        return Object.keys(playlist)[0];
+      },
+      getPlaylistContent(playlist) {
+        return Object.values(playlist)[0];
+      },
+    }
   };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @import '@/assets/sass/styles.scss';
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
 
+  .playlist-name{
+    font-weight: bolder;
+    color: $primary;
+    padding-top: 30px;
+    font-size: 18px;
+  }
+
+  .check-icon {
+    float: left;
+    padding: 0 10px 0 0;
+    font-size: 10px;
+    margin-top: 8px;
+  }
+
+  .plus-icon{
+    padding: 0;
+    background-color: transparent;
+    color:white;
+    border: none;
+  }
+
+  .column.song-name{
+    font-size: 18px;
+    padding: 0 0 0 20px;
+  }
+
+  .song-info{
+    padding-left:20px;
+    font-size: 14px;
+    color: grey;
+  }
+
+  .playlist-content{
+    margin-left: 30px
+  }
 </style>
