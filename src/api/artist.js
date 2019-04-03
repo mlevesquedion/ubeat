d4b40d431b09b8b23ff85a66ab9384e5f43dcf84
Bruce Perry <qcepicbruce@hotmail.com>
2019-04-03 14:49:23 -0400

    merged requests

--- src/api/artist.js
+++ src/api/artist.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
-import { apiRoot } from './constants';
+import {
+  apiRoot,
+  apiKeyParam,
+  lastFmApiRoot,
+  lastFmGetArtistInfo,
+  lastFmJsonFormat
+} from './constants';
 import Artist from '../models/artist';
 import ArtistAlbums from '../models/artistAlbums';
 import DetailArtist from '../models/detailArtist';
 
 const artistRoot = `${apiRoot}artists/`;
-const API_KEY = 'a8090a6b57c8d1fc53f6fdb6bd660e2b';
-
-const get = id =>
-  axios
-    .get(`${artistRoot}${id}`)
-    .then(({ data }) => Artist.from(data.results[0]));
 
 const getAlbums = id =>
   axios
@@ -20,38 +20,20 @@ const getAlbums = id =>
 const getDetails = artistName =>
   axios
     .get(
-      `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=${API_KEY}&format=json`
+      lastFmApiRoot +
+        lastFmGetArtistInfo +
+        artistName +
+        apiKeyParam +
+        lastFmJsonFormat
     )
     .then(({ data }) => DetailArtist.from(data.artist));
 
-// const getAll = (id, name) => {
-//   axios
-//     .all([
-//       axios.get(`${artistRoot}${id}`),
-//       axios.get(
-//         `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${name}&api_key=${API_KEY}&format=json`
-//       )
-//     ])
-//     .then(
-//       axios.spread((ubeatRes, lastFmRes) => {
-//         Artist.from(ubeatRes.data.results[0]);
-//         DetailArtist.from(lastFmRes.data.artist);
-//       })
-//     );
-// };
-
-// const get = id =>
-//   axios
-//     .get(`${artistRoot}${id}`)
-//     .then(({ data }) => {
-//       Artist.from(data.results[0]);
-//       return axios.get(
-//         `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${
-//           data.results[0].artistName
-//         }&api_key=${API_KEY}&format=json`
-//       );
-//     })
-//     .then(({ data }) => DetailArtist.from(data.artist));
+const get = id =>
+  axios.get(`${artistRoot}${id}`).then(({ data }) => {
+    const promise1 = Artist.from(data.results[0]);
+    const promise2 = getDetails(data.results[0].artistName);
+    return Promise.all([promise1, promise2]).then(values => values);
+  });
 
 export default {
   get,
--- src/components/artist/ArtistInfoView.vue
+++ src/components/artist/ArtistInfoView.vue
@@ -1,10 +1,16 @@
 <template>
   <section class="section">
     <div class="container has-text-centered">
-      <p class="label is-size-1">{{ artist.name }}</p>
-      <p class="label is-size-3">{{ artist.genre }}</p>
-      <DetailArtist :artistName="artist.name"></DetailArtist>
-      <a :href="artist.url" id="itunes-link">
+      <p class="label is-size-1">{{ artist[0].name }}</p>
+      <p class="label is-size-3">{{ artist[0].genre }}</p>
+
+      <img
+        :alt="artist[0].name"
+        class="responsive-image is-round"
+        :src="artist[1].image"
+      />
+      <p class="label">{{ artist[1].bio }}</p>
+      <a :href="artist[0].url" id="itunes-link">
         <img
           alt="Listen on Apple Music"
           src="https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=&kind=artist&bubble=apple_music"
