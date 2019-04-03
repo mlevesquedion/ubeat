8f65705f061e104d23bf273c3ebbb5e56022b4dc
Bruce Perry <qcepicbruce@hotmail.com>
2019-04-03 11:26:21 -0400

    tried merging get and getDetails

--- src/api/artist.js
+++ src/api/artist.js
@@ -24,6 +24,35 @@ const getDetails = artistName =>
     )
     .then(({ data }) => DetailArtist.from(data.artist));
 
+// const getAll = (id, name) => {
+//   axios
+//     .all([
+//       axios.get(`${artistRoot}${id}`),
+//       axios.get(
+//         `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${name}&api_key=${API_KEY}&format=json`
+//       )
+//     ])
+//     .then(
+//       axios.spread((ubeatRes, lastFmRes) => {
+//         Artist.from(ubeatRes.data.results[0]);
+//         DetailArtist.from(lastFmRes.data.artist);
+//       })
+//     );
+// };
+
+// const get = id =>
+//   axios
+//     .get(`${artistRoot}${id}`)
+//     .then(({ data }) => {
+//       Artist.from(data.results[0]);
+//       return axios.get(
+//         `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${
+//           data.results[0].artistName
+//         }&api_key=${API_KEY}&format=json`
+//       );
+//     })
+//     .then(({ data }) => DetailArtist.from(data.artist));
+
 export default {
   get,
   getAlbums,
