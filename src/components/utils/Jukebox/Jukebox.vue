84386aadbb9315cb2aa9af478113389ec2112850
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-04-09 22:42:16 -0400

    Jukebox

---
+++ src/components/utils/Jukebox/Jukebox.vue
@@ -0,0 +1,41 @@
+<template>
+  <fragment>
+    <slot :playingTrackId="playingTrackId"></slot>
+  </fragment>
+</template>
+
+<script>
+import Jukebox from './jukebox';
+
+export default {
+  name: 'Jukebox',
+  data() {
+    return {
+      jukebox: new Jukebox(this.stop, this.onPlaybackError),
+      playingTrackId: null
+    };
+  },
+  mounted() {
+    this.$root.$on('play-track', this.play);
+    this.$root.$on('stop-track', this.stop);
+  },
+  beforeDestroy() {
+    this.stop();
+  },
+  methods: {
+    play(track) {
+      this.jukebox.play(track);
+      this.playingTrackId = this.jukebox.playingTrackId();
+    },
+    stop() {
+      this.jukebox.stop();
+      this.playingTrackId = null;
+    },
+    onPlaybackError(track) {
+      this.$toasted.show(`Could not play track ${track.name}!`, {
+        type: 'ubeat-error'
+      });
+    }
+  }
+};
+</script>
