f7261501994879d6fdd5cffa85cbc3f0a74b7f78
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-04-11 19:05:29 -0400

    Change 'by' to '-'

---
+++ src/components/album/TrackDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="is-clipped">
+    <router-link
+      class="is-primary link"
+      :to="{ name: 'Album', params: { id: track.olbumId } }"
+    >
+      {{ track.name }}
+    </router-link>
+    -
+    <router-link
+      class="is-primary link"
+      :to="{ name: 'Artist', params: { id: track.artistId } }"
+      >{{ track.artist }}
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TrackDetails',
+  props: ['track']
+};
+</script>
+
+<style scoped lang="scss">
+.is-clipped {
+  min-width: 100px;
+  max-width: 700px;
+  width: 50vw;
+}
+</style>
